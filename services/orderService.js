2a177537b412009a3fe81cbb2bc6b058e07a4765
Ahmed Abd Elaziz <ahmedabdelaziz500@gmail.com>
2023-05-14 18:23:50 +0300

    Debugging webhook bug v2.7

--- services/orderService.js
+++ services/orderService.js
@@ -142,7 +142,7 @@ exports.webhookCheckout = asyncHandler(async (req, res, next) => {
     let event
 
     try {
-        event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+        event = stripe.webhooks.constructEvent(req.rawBody, sig, process.env.STRIPE_WEBHOOK_SECRET);
     } catch (err) {
         return res.status(400).send(`Webhook Error: ${err.message}`)
     }
